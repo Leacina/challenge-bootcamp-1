966582cd7c48729b33abf80ccc49343fb488f02d
Giovane <giovaneleacina@unesc.net>
2020-08-23 01:29:04 -0300

    Finalizado atividade da 1 semana do Bootcamp da rockeseat

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { v4: uuid } = require('uuid');
+const { v4: uuid } = require('uuid');
 
 const app = express();
 
@@ -10,24 +10,97 @@ app.use(cors());
 
 const repositories = [];
 
+/**
+ * Retorna todos os dados
+ */
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
 
+/**
+ * Insere um novo repositorie
+ */
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+
+  const repositorie = {
+    id: uuid(),
+    title, 
+    url,
+    techs,
+    likes: 0
+  }
+
+  repositories.push(repositorie);
+
+  return response.json(repositorie);
 });
 
+/**
+ * Altera um repositorie caso encontrar
+ */
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+  const { id } = request.params;
+
+  const repositorieIndex = repositories.findIndex(repositorie => repositorie.id = id);
+
+  if(repositorieIndex < 0){
+    return response.status(400).json({
+      error:"repositorie not found"
+    });
+  };
+
+  const repositorie = {
+    id,
+    title,
+    url,
+    techs,
+    likes: repositories[repositorieIndex].likes
+  }
+
+  repositories[repositorieIndex] = repositorie;
+
+  return response.json(repositorie);
+
 });
 
+/**
+ * Deleta um repositorie caso encontrar
+ */
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  repositorieIndex = repositories.findIndex(repositorie => repositorie.id == id);
+
+  if(repositorieIndex < 0){
+    return response.status(400).json({
+      error:"repositorie not found"
+    });
+  };
+
+  repositories.splice(repositorieIndex, 1);
+
+  return response.status(204).send();
 });
 
+/**
+ * Adiciona um like no repositorie a cada chamada da api
+ */
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  repositorieIndex = repositories.findIndex(repositorie => repositorie.id == id);
+
+  if(repositorieIndex < 0){
+    return response.status(400).json({
+      error:"repositorie not found"
+    });
+  };                                                                                             
+
+  repositories[repositorieIndex].likes += 1;
+
+  return response.status(204).send();
 });
 
 module.exports = app;
